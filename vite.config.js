b37b3df754a8ff404e797ef67bac090dcfc86017
AdrianLPZ <40033290+AdrianLPZ@users.noreply.github.com>
2023-07-14 11:55:50 -0600

    Se agrego el nombre del repo al vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Nombre del repo/",
+  base: "/5-dev-jobs/",
   plugins: [react()],
 })
